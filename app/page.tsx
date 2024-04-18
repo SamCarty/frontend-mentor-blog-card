31155c4eed513673c71d906be3d85de95083ee11
Sam Carty <sam.carty@coop.co.uk>
2024-04-18 15:50:47 +0100

    Update card wrapper styling

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,8 @@ import Card from '@/components/Card'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-8">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        <Card />
-        <Card />
-        <Card />
-      </div>
+    <main className="flex min-h-screen flex-col items-center justify-center p-8">
+      <Card />
     </main>
   )
 }
